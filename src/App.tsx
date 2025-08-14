f76877e56216ff978a61e77c31827adbb9c20781
GitLakshman <lakshmanprasad13579@gmail.com>
2025-08-14 20:39:54 +0530

    theme is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/navbar";
 
 const App = () => {
   return (
@@ -8,17 +9,13 @@ const App = () => {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area={"nav"} bg="orange.300">
-        Nav
+      <GridItem area={"nav"}>
+        <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"} bg="blue.300">
-          Aside
-        </GridItem>
+        <GridItem area={"aside"}>Aside</GridItem>
       </Show>
-      <GridItem area={"main"} bg="green.300">
-        Main
-      </GridItem>
+      <GridItem area={"main"}>Main</GridItem>
     </Grid>
   );
 };
