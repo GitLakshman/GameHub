2729c29a0465ec5dc8261e1ded74fbb769fe6c33
GitLakshman <lakshmanprasad13579@gmail.com>
2025-08-21 21:24:56 +0530

    build search input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,25 @@
-import { HStack, Image } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import { BsSearch } from "react-icons/bs";
 const Navbar = () => {
   return (
-    <HStack justifyContent={"space-between"} p={"10px"}>
+    <HStack p={"10px"}>
       <Image src={logo} boxSize="60px" />
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          borderRadius={20}
+          variant="filled"
+          placeholder="Search games ...."
+        />
+      </InputGroup>
       <ColorModeSwitch />
     </HStack>
   );
