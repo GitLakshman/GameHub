b3ebba11f2473e1abffeb4473317f2f6e8c71768
GitLakshman <lakshmanprasad13579@gmail.com>
2025-08-25 14:31:52 +0530

    add GameHeader

---
+++ src/components/GameHeader.tsx
@@ -0,0 +1,18 @@
+import { Heading } from "@chakra-ui/react";
+import { GameQuery } from "../App";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+
+const GameHeader = ({ gameQuery }: Props) => {
+  const heading = `${gameQuery.platform?.name || ""} 
+  ${gameQuery.genre?.name || ""} Games`;
+  return (
+    <Heading as={"h1"} paddingY={5}>
+      {heading}
+    </Heading>
+  );
+};
+
+export default GameHeader;
