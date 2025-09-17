0e02a7b450fed308a470c3517590641a3d09449b
GitLakshman <lakshmanprasad13579@gmail.com>
2025-09-17 11:31:12 +0530

    Updated usePlatform hook in necessary imports

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -3,7 +3,7 @@ import platforms from "../data/platforms";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
 
-export interface Platfrom {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
@@ -14,7 +14,7 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: () =>
       apiClient
-        .get<FetchResponse<Platfrom>>("/platforms/lists/parents")
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
         .then((res) => res.data),
     staleTime: 24 * 60 * 60 * 1000,
     initialData: { count: platforms.length, results: platforms },
