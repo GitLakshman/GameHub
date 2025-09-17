91e00ad96e9b5115b22627f0b8b0181b32bd3e98
GitLakshman <lakshmanprasad13579@gmail.com>
2025-09-17 11:17:07 +0530

    Refactoring useGames hook with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,9 @@
 import axios, { CanceledError } from "axios";
 
-export { CanceledError };
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
